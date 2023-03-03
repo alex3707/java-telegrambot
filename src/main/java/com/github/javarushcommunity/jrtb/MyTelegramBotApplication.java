5b1f9609f02b89c08a2a40e407740e9a2d6dc5f2
Alex3707 <alex3707@mail.ru>
2023-03-03 21:38:47 +0300

    bot echo, yes

--- src/main/java/com/github/javarushcommunity/jrtb/MyTelegramBotApplication.java
+++ src/main/java/com/github/javarushcommunity/jrtb/MyTelegramBotApplication.java
@@ -2,12 +2,12 @@ package com.github.javarushcommunity.jrtb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// для комита
+
 @SpringBootApplication
-public class JavarushTelegramBotApplication {
+public class MyTelegramBotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavarushTelegramBotApplication.class, args);
+		SpringApplication.run(MyTelegramBotApplication.class, args);
 	}
 
 }
